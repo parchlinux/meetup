6a408f23b96d2933b531892eeb64a024a379038c
Ali Almasi <a710almasi@gmail.com>
2024-09-30 00:54:20 +0330

    dobare kobidim as aval sakhtim

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,37 +14,39 @@ const meetupDate = document.querySelector(".next-meetup__date");
 const meetupPlace = document.querySelector(".next-meetup__place");
 const meetupLink = document.getElementById("join-meetup");
 
-fetch("./upcoming.json")
-	.then((response) => response.json())
-	.then((meetup) => {
-		if (meetup.planned) {
-			meetupTitle.textContent = meetup.title;
-			meetupTime.textContent = meetup.time;
-			meetupDate.textContent = meetup.date;
-			meetupPlace.textContent = meetup.place;
-			let linkNotReady =
-				meetup.link.length <= 0 || meetup.link.includes("meetup.parchlinux");
-			meetupLink.textContent = linkNotReady
-				? "بزودی لینک قرار خواهد گرفت 🕒"
-				: "لینک ورود به جلسه";
-			meetupLink.addEventListener("click", () => {
-				!linkNotReady
-					? (window.location.href = meetup.link)
-					: alert("هنوز لینکی برای جلسه آماده نشده. لطفا بعدا بررسی کنید.");
-			});
-		} else {
-			meetupTitle.textContent = "فعلا مشخص نشده";
-			meetupTime.textContent = "فعلا مشخص نشده";
-			meetupDate.textContent = "فعلا مشخص نشده";
-			meetupPlace.textContent = "فعلا مشخص نشده";
-			meetupLink.textContent = "بزودی مشخص خواهد شد";
-			meetupLink.addEventListener("click", () =>
-				alert("هنوز جلسه بعدی مشخص نشده. ⚠️")
-			);
-			document.querySelector(".next-meetup__details").style.display = "none";
-		}
-	})
-	.catch((err) => {
-		console.error(err);
-		alert("Error while fetching meetup information. Check the console.");
-	});
+const fetchMeetupInfo = () =>
+	fetch("./upcoming.json")
+		.then((response) => response.json())
+		.then((meetup) => {
+			if (meetup.planned) {
+				meetupTitle.textContent = meetup.title;
+				meetupTime.textContent = meetup.time;
+				meetupDate.textContent = meetup.date;
+				meetupPlace.textContent = meetup.place;
+				let linkNotReady =
+					meetup.link.length <= 0 || meetup.link.includes("meetup.parchlinux");
+				meetupLink.textContent = linkNotReady
+					? "بزودی لینک قرار خواهد گرفت 🕒"
+					: "لینک ورود به جلسه";
+				meetupLink.addEventListener("click", () => {
+					!linkNotReady
+						? (window.location.href = meetup.link)
+						: alert("هنوز لینکی برای جلسه آماده نشده. لطفا بعدا بررسی کنید.");
+				});
+			} else {
+				meetupTitle.textContent = "فعلا مشخص نشده";
+				meetupTime.textContent = "فعلا مشخص نشده";
+				meetupDate.textContent = "فعلا مشخص نشده";
+				meetupPlace.textContent = "فعلا مشخص نشده";
+				meetupLink.textContent = "بزودی مشخص خواهد شد";
+				meetupLink.setAttribute("disabled", "true");
+				meetupLink.addEventListener("click", () =>
+					alert("هنوز جلسه بعدی مشخص نشده. ⚠️")
+				);
+				document.querySelector(".next-meetup__details").style.display = "none";
+			}
+		})
+		.catch((err) => {
+			console.error(err);
+			alert("Error while fetching meetup information. Check the console.");
+		});
